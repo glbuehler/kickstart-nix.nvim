d7732ee3b7403c6d4c7c3b8dae359618a5b8d9f1
Gideon Bühler <gideonbuehler18@gmail.com>
2025-05-09 21:34:55 +0200

    cleaned up keymaps

--- nvim/init.lua
+++ nvim/init.lua
@@ -22,12 +22,11 @@ opt.incsearch = true
 opt.hlsearch = true
 
 opt.spell = true
-opt.spelllang = 'en'
 
 opt.expandtab = true
-opt.tabstop = 2
-opt.softtabstop = 2
-opt.shiftwidth = 2
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
 opt.foldenable = true
 opt.history = 2000
 opt.nrformats = 'bin,hex' -- 'octal'
